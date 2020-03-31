da0cfa6be85934085c6559c21b931e7fd7c3c620
unurgunite <senpaiguru1488@gmail.com>
2020-03-31 11:55:07 +0300

    rearrange classes for icons

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def main
     @list = ['number system', 'length', 'weight', 'time']
-    @favicon = %w[fa-home fa-user fa-address-card fa-project-diagram fa-blog fa-address-book fa-map-pin]
+    @favicon = %w[fa-home fa-ruler-vertical fa-weight-hanging fa-clock]
   end
 end
