2a001c265d8c5547eadbc5db7124c5e12d3a0b23
unurgunite <senpaiguru1488@gmail.com>
2020-04-01 20:07:25 +0300

    create meta-tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   set_meta_tags title: 'Converter',
                 description: 'This site was created as online free converter',
-                keywords: %w[abc def ghi],
+                keywords: %w[Online Converter],
                 author: 'Derek2',
                 follow: true,
                 separator: '|',
