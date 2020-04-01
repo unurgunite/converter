6625fa1ee078c49f56d833d255ca4e97b6eb1e5c
unurgunite <senpaiguru1488@gmail.com>
2020-04-01 11:11:34 +0300

    rearrange arras with favicon values

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def main
-    @list = ['number system', 'length', 'weight', 'time']
-    @favicon = %w[fa-home fa-ruler-vertical fa-weight-hanging fa-clock]
+    @list = ['Home page', 'number system', 'length', 'weight', 'time']
+    @favicon = %w[fa-home fa-sort-numeric-down-alt fa-ruler-vertical fa-weight-hanging fa-clock]
   end
 end
