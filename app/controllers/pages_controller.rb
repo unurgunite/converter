d3deec995c8e5583913cd2803ce070d4e0d2a130
unurgunite <senpaiguru1488@gmail.com>
2020-04-01 11:31:10 +0300

    remove arrays and replace them with favicon constant

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,3 @@
 class PagesController < ApplicationController
-  def main
-    @list = ['Home page', 'number system', 'length', 'weight', 'time']
-    @favicon = %w[fa-home fa-sort-numeric-down-alt fa-ruler-vertical fa-weight-hanging fa-clock]
-  end
+  def main; end
 end
