d987f3bf05bea3f065ac3683460775248ae2f2da
unurgunite <senpaiguru1488@gmail.com>
2020-11-07 20:04:38 +0300

    some minor changes

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 module PagesHelper
-  #   FAVICON = {
-  #     'Home page': 'fa-home',
-  #     'Number system': 'fa-sort-numeric-down-alt',
-  #     'Length': 'fa-ruler-vertical',
-  #     'Weight': 'fa-weight-hanging',
-  #     'Time': 'fa-clock'
-  #   }.freeze
 
   FAVICON = [{ 'link_name': 'Home page', 'class_name': 'fa-home', 'path': '/' },
              { 'link_name': 'Number system', 'class_name': 'fa-sort-numeric-down-alt', 'path': 'number' },
